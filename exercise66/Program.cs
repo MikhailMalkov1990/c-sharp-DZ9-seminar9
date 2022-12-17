e1c53ca9af8bcfd6d862e4b3fb0acd9bc75f2a41
mikhail_malkov <mikhailmalkov9@gmail.com>
2022-12-16 18:43:50 -0600

    commited exercise66

---
+++ exercise66/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// Выполнить с помощью рекурсии
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("even num M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("even num N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(SummaDigits(m, n));
+
+int SummaDigits(int num1, int num2)
+{
+    if (num1 == num2) return num1;
+    else if (num1 < num2) return num2 + SummaDigits(num1, num2 - 1);
+    else return num2 + SummaDigits(num1, num2 + 1);
+}
